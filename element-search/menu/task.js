88aa48995bc18e4bbd8c07b0cf55e502d2fe95a2
Ulitka62 <vadborisov62@gmail.com>
2022-08-26 11:17:41 +0300

    fixed errors 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,7 +1,19 @@
 const menuLink = Array.from(document.getElementsByClassName('menu__link'))
 for(let item of menuLink) {
     item.onclick = function() {
+        const menuMain = this.closest('ul');
         const parent = this.parentElement.querySelector('.menu_sub');
+        
+        if( menuMain.className === 'menu menu_main') {
+            const active = menuMain.querySelector('.menu_active');
+            if(active && (active !== parent))
+            active.className = 'menu menu_sub'
+        }
+        
+        if(!parent) {
+            return true
+        }
+
         if(parent.className === 'menu menu_sub') {
             parent.className = 'menu menu_sub menu_active'
         } else {
